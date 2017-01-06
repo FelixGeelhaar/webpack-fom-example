fbf5a77d3fe8df441f396845425d1cb05a8daafe
Felix Geelhaar <felix@felixgeelhaar.de>
2017-01-06 19:46:43 +0100

    Install and configure html-webpack-plugin, also change index.html to index.ejs to enable the html-webpack-plugin to inject the title.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   context: path.resolve(__dirname, 'src'),
@@ -6,7 +7,13 @@ const config = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.ejs',
+      title: 'Webpack FOM Example'
+    })
+  ]
 };
 
 module.exports = config;
