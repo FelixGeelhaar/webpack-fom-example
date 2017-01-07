d668be5d6707ad07e4f4e02b79510b384bcd2602
Felix Geelhaar <felix@felixgeelhaar.de>
2017-01-07 23:24:11 +0100

    Installed and configure webpack image loaders and font loaders. Also installed and configured react with react dependencies and configured chunks (vendor) in webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
@@ -6,11 +7,20 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // Could be done through webpack -p or NODE_ENV=production webpack
 const isProduction = process.env.NODE_ENV === 'production';
 
+const VENDOR_LIBS = [
+  'react', 'react-dom', 'react-router', 'lodash',
+  'axios'
+];
+
 const config = {
-  entry: './src/index.js',
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    bundle: './index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     rules: [
@@ -25,15 +35,37 @@ const config = {
         use: 'babel-loader',
         test: /\.js$/,
         exclude: /node_modules/
+      },
+      {
+        use: [
+          {
+            loader: 'file-loader',
+            options: { name: 'fonts/[hash].[ext]'}
+          }
+        ],
+        test: /\.(ttf|woff|woff2|eot)$/
+      },
+      {
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 40000 }
+          },
+          'image-webpack-loader'
+        ],
+        test: /\.(jpe?g|png|gif|svg)/
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.ejs',
+      template: './index.ejs',
       title: 'Webpack FOM Example'
     }),
     new ExtractTextPlugin('style.css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['vendor', 'manifest']
+    }),
     // Production configuration
     isProduction ?
       new webpack.optimize.UglifyJsPlugin({
